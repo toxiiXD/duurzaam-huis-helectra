f64983cd842504fe8dce5dc03d302a29143c1efd
Rosario <40689@ma-web.nl>
2026-06-02 09:48:37 +0200

    replace html, php basic site

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to my website</h1>
+        <p>This is a simple webpage created using HTML and CSS.</p>
+        <button class="btn">Click Me</button>
+    </div>
+</body>
+</html>
