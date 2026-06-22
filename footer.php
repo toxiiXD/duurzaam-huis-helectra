7c68113edbd5360eb3d24b20408d58dda078bbfe
toxiiXD <165729191+toxiiXD@users.noreply.github.com>
2026-06-22 11:23:36 +0200

    dadQD

--- footer.php
+++ footer.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
     <div>
-        
+            <footer>
+                <p>hellectra- damian rosario moiza</p>
+            </footer>
     </div>
-</body>
-</html>
--- header.php
+++ header.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>
-        
-    </div>
-    
-</body>
-</html>
+        <header>
+            <h1>hellectra</h1>
+            <IMG src="hellectra idk.png" alt="Logo"></IMG>
+        </header>
