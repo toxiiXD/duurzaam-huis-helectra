72379214b350260ac36d3513ff3f24029554d987
moiza tahir <34110@ma-web.nl>
2026-06-25 10:56:43 +0200

    commit-not-finished-yet

--- index.php
+++ index.php
@@ -40,6 +40,9 @@
             </div>
         </section>
         <section class="temp-item"><h3>temperatuur </h3></section>
+                    <div class="chart-card-temp">
+                <canvas id="temperatuurChart"></canvas>
+            </div>
     </main>
     <?php include_once 'footer.php'; ?>
 </body>
